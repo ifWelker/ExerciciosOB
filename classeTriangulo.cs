ad1d9db952c3206d6d8e815afc80f1116f621156
ifWelker <84274394+ifWelker@users.noreply.github.com>
2022-11-16 22:00:14 -0300

    Create classeTriangulo.cs

---
+++ classeTriangulo.cs
@@ -0,0 +1,11 @@
+//Classe utilizada para o exercicio do calculo da area do Triangulo.
+
+namespace CursoCSharp
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
