f67b9d08cfc62edd2efc556f4eb05995c2309cd4
ifWelker <84274394+ifWelker@users.noreply.github.com>
2022-11-23 20:32:53 -0300

    Create classeFuncionario.cs

---
+++ classeFuncionario.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
