34638b15e53b2f4932abed2615e5c436fbebda84
ifWelker <84274394+ifWelker@users.noreply.github.com>
2022-11-23 21:22:44 -0300

    Update classeTriangulo.cs

--- classeTriangulo.cs
+++ classeTriangulo.cs
@@ -1,11 +1,25 @@
 //Classe utilizada para o exercicio do calculo da area do Triangulo.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CursoCSharp
 {
-    class Triangulo
+    internal class Triangulo
     {
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+
     }
 }
